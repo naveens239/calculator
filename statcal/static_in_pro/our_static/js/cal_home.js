6b535e7066406c4577cacb5728c1d7b649fb6956
Naveena <ndavids239@gmail.com>
2018-02-06 17:49:40 +0530

    JS changes

--- statcal/static_in_pro/our_static/js/cal_home.js
+++ statcal/static_in_pro/our_static/js/cal_home.js
@@ -1,4 +1,3 @@
-
 //to prevent auto scroll from top
 $("#calDForm").submit(function(e) {
     e.preventDefault();
@@ -22,13 +21,13 @@ function dcalFunction(){
     var c2c1 = document.getElementById("dc2c1").value;//visitors-variation
     var c1c2 = document.getElementById("dc1c2").value;//conversion-control
     var c2c2 = document.getElementById("dc2c2").value;//conversion-variation
-    document.getElementById("mc1c1").value=c1c1
-    document.getElementById("mc2c1").value=c2c1
-    document.getElementById("mc1c2").value=c1c2
-    document.getElementById("mc2c2").value=c2c2
+    document.getElementById("mc1c1").value=c1c1;
+    document.getElementById("mc2c1").value=c2c1;
+    document.getElementById("mc1c2").value=c1c2;
+    document.getElementById("mc2c2").value=c2c2;
     console.log(c1c1,c2c1,c1c2,c2c2);
-    if (c1c1==null || c1c1=="" || typeof c1c1 ==='number', c1c2==null ||c1c2=="" || typeof c1c2 ==='number',
-     c2c1==null || c2c1==""|| typeof c2c1 ==='number', c2c2==null ||c2c2 ==""|| typeof c2c2 ==='number'){
+    if (c1c1===null || c1c1==="" || typeof c1c1 ==='number', c1c2===null ||c1c2==="" || typeof c1c2 ==='number',
+     c2c1===null || c2c1===""|| typeof c2c1 ==='number', c2c2===null ||c2c2 ===""|| typeof c2c2 ==='number'){
         alert("Please enter numbers in all control and variation fields.");
         return;
     }
@@ -37,20 +36,21 @@ function dcalFunction(){
         return;
     }
     else{
+        var p_value;
         $.ajax({
         url: "calculateSignify/",
         data : { 'c1c1':c1c1,'c1c2':c1c2,'c2c1':c2c1,'c2c2':c2c2},
         success : function(result) {
             if( p_value ==='NaN'){
-                var p_value = 'NaN';
+                p_value = 'NaN';
                 //console.log(result.p_value.toFixed(3), result.signify);
             }
             else{
                  
-                var p_value = parseFloat(result.p_value).toFixed(3);
+                p_value = parseFloat(result.p_value).toFixed(3);
             }
 
-            var signify = result.signify
+            var signify = result.signify;
             console.log('here');
            
             document.getElementById('dpvalue').innerHTML=p_value;
@@ -59,7 +59,7 @@ function dcalFunction(){
             document.getElementById('msignify').innerHTML=signify;
             console.log("requested access complete");
         }
-    })
+    });
     }
     document.getElementById('resultDiv').style.display="block";
 }
@@ -69,13 +69,13 @@ function mcalFunction(){
     var c2c1 = document.getElementById("mc2c1").value;//visitors-variation
     var c1c2 = document.getElementById("mc1c2").value;//conversion-control
     var c2c2 = document.getElementById("mc2c2").value;//conversion-variation
-    document.getElementById('dc1c1').value=c1c1
-    document.getElementById('dc2c1').value=c2c1
-    document.getElementById('dc1c2').value=c1c2
-    document.getElementById('dc2c2').value=c2c2
+    document.getElementById('dc1c1').value=c1c1;
+    document.getElementById('dc2c1').value=c2c1;
+    document.getElementById('dc1c2').value=c1c2;
+    document.getElementById('dc2c2').value=c2c2;
     console.log(c1c1,c2c1,c1c2,c2c2);
-    if (c1c1==null || c1c1=="" || typeof c1c1 ==='number', c1c2==null ||c1c2=="" || typeof c1c2 ==='number',
-     c2c1==null || c2c1==""|| typeof c2c1 ==='number', c2c2==null ||c2c2 ==""|| typeof c2c2 ==='number'){
+    if (c1c1===null || c1c1==="" || typeof c1c1 ==='number', c1c2===null ||c1c2==="" || typeof c1c2 ==='number',
+     c2c1===null || c2c1===""|| typeof c2c1 ==='number', c2c2===null ||c2c2 ===""|| typeof c2c2 ==='number'){
         alert("Please enter numbers in all control and variation fields.");
         return;
     }
@@ -84,20 +84,21 @@ function mcalFunction(){
         return;
     }
     else{
+        var p_value;
         $.ajax({
         url: "calculateSignify/",
         data : { 'c1c1':c1c1,'c1c2':c1c2,'c2c1':c2c1,'c2c2':c2c2},
         success : function(result) {
             if( p_value ==='NaN'){
-                var p_value = 'NaN';
+                p_value = 'NaN';
                 //console.log(result.p_value.toFixed(3), result.signify);
             }
             else{
                  
-                var p_value = parseFloat(result.p_value).toFixed(3);
+                p_value = parseFloat(result.p_value).toFixed(3);
             }
 
-            var signify = result.signify
+            var signify = result.signify;
             console.log('here');
             document.getElementById('mpvalue').innerHTML=p_value;
             document.getElementById('msignify').innerHTML=signify;
@@ -106,7 +107,7 @@ function mcalFunction(){
             console.log("requested access complete");
 
         }
-    })
+    });
     }
       document.getElementById('resultDiv').style.display="block";
 }
